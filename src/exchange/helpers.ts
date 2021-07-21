d019b5706c8aa2f4e2dc06b9b01d0142c1baf0b9
0xbatman <0xbatman@xyz.com>
2021-07-21 11:12:48 +0700

    add kcc configuration

--- src/exchange/helpers.ts
+++ src/exchange/helpers.ts
@@ -11,13 +11,13 @@ export let MIN_LIQUIDITY_BD = BigDecimal.fromString("1");
 export let MIN_RESERVE_UPDATE_BD = BigDecimal.fromString("10");
 
 export let HOPE: string = "0xd78c475133731cd54dadcb430f7aae4f03c1e660";
-export let USD: string = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174"; // USDC
-export let USDT: string = "0xc2132d05d31c914a87c6611c10748aeb04b58e8f"; // USDT
+export let USD: string = "0x980a5afef3d17ad98635f6c5aebcbaeded3c3430"; // USDC
+export let USDT: string = "0x0039f574ee5cc39bdd162e9a88e3eb1f111baf48"; // USDT
 export let DAI: string = "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063"; // DAI
-export let MATIC: string = "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270";
+export let KCS: string = "0x4446fc4eb47f2f6586f9faab68b3498f86c07521";
 export let WETH: string = "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619";
 export let STABLES: string[] = [DAI, USD, USDT];
-export let WHITELIST: string[] = [HOPE, WETH, MATIC, DAI, USD, USDT];
+export let WHITELIST: string[] = [HOPE, WETH, KCS, DAI, USD, USDT];
 export let SAVE_SWAP_FROM = BigInt.fromI32(0);
 export function getExchange(id: string): Exchange | null {
   let factory = Exchange.load(id);
@@ -251,10 +251,10 @@ export function updatePoolLiquidity(pool: Pool): void {
     poolLiquidity = usdPoolToken.balance.div(usdPoolToken.denormWeight).times(poolInfo.totalWeight);
     hasPrice = true;
     hasUsdPrice = true;
-  } else if (tokensList.includes(Address.fromString(MATIC))) {
-    let maticToken = Token.load(MATIC);
+  } else if (tokensList.includes(Address.fromString(KCS))) {
+    let maticToken = Token.load(KCS);
     if (maticToken !== null && maticToken.priceUSD.gt(ZERO_BD)) {
-      let poolTokenId = id.concat("-").concat(MATIC);
+      let poolTokenId = id.concat("-").concat(KCS);
       let poolToken = PoolToken.load(poolTokenId);
       poolLiquidity = maticToken.priceUSD
         .times(poolToken.balance)
@@ -291,7 +291,7 @@ export function updatePoolLiquidity(pool: Pool): void {
   let denormWeight = ZERO_BD;
 
   let poolTokens: Array<PoolToken> = [];
-  let isPoolMaticStable = tokensList.includes(Address.fromString(MATIC)) && tokensList.includes(Address.fromString(USD)); //only update MATIC by pool MATIC-USDC
+  let isPoolMaticStable = tokensList.includes(Address.fromString(KCS)) && tokensList.includes(Address.fromString(USD)); //only update KCS by pool KCS-USDC
   let isPoolWETHStable = tokensList.includes(Address.fromString(WETH)) && tokensList.includes(Address.fromString(USD)); //only update WETH by pool WETH-USDC
   let tokens: Array<Token> = [];
   for (let i: i32 = 0; i < tokensList.length; i++) {
@@ -302,7 +302,7 @@ export function updatePoolLiquidity(pool: Pool): void {
     if (hasPrice) {
       if (token.poolTokenId == poolTokenId || poolLiquidity.gt(token.poolLiquidity)) {
         if (
-          (tokenId != MATIC.toString() || (tokenId == MATIC.toString() && isPoolMaticStable)) &&
+          (tokenId != KCS.toString() || (tokenId == KCS.toString() && isPoolMaticStable)) &&
           (tokenId != WETH.toString() || (tokenId == WETH.toString() && isPoolWETHStable))
         ) {
           if (poolLiquidity.gt(MIN_LIQUIDITY_BD)) {
